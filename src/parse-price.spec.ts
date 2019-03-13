73368a405bb822258672d8d773241e0e03b2db4d
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-13 21:59:32 +0100

    Added additional empty specs with currency & currency symbol.

--- src/parse-price.spec.ts
+++ src/parse-price.spec.ts
@@ -25,4 +25,12 @@ describe('parsePrice', () => {
 
     });
 
+    xit('should parse price with currency', () => {
+        // @TODO: 12.12EUR => {coefficient: 1212, exponent: -2, currency: 'EUR'}
+    });
+
+    xit('should parse price with currency symbol', () => {
+        // @TODO: 12.12€ => {coefficient: 1212, exponent: -2, currency: 'EUR'}
+    });
+
 });
