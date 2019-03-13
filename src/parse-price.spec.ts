69953c770e00d02bbc40a341a6d40fd6f6ee1fd8
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-13 21:26:56 +0100

    Price parser.

--- src/parse-price.spec.ts
+++ src/parse-price.spec.ts
@@ -1,8 +1,19 @@
+/**
+ * @deprecated wip
+ * @param priceString
+ */
+function parsePrice(priceString: string) {
+    throw new Error('😱 Not implemented yet!');
+}
 
 describe('parsePrice', () => {
 
     xit('should parse price without currency', () => {
-        // @todo: parsePrice('12.12') => {coefficient: 1212, exponent: -2, currency: null}
+        expect(parsePrice('12.12')).toEqual({
+            coefficient: 1212,
+            exponent: -2,
+            currency: null
+        });
     });
 
     xit('should parse price with currency code', () => {
