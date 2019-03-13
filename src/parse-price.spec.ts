a9bcbe8c7a38fd2308cd1ef467f8f4112204903a
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-13 21:23:13 +0100

    Price parser.

--- src/parse-price.spec.ts
+++ src/parse-price.spec.ts
@@ -2,11 +2,11 @@
 describe('parsePrice', () => {
 
     xit('should parse price without currency', () => {
-
+        // @todo: parsePrice('12.12') => {coefficient: 1212, exponent: -2, currency: null}
     });
 
     xit('should parse price with currency code', () => {
-
+        // @todo: parsePrice('12.12EUR') => {coefficient: 1212, exponent: -2, currency: 'EUR'}
     });
 
     xit('should parse price with currency symbol', () => {
