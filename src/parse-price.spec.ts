0ba4c4f58d4ce81770b022c972925643f459c92a
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-13 21:57:38 +0100

    Added parse price without currency spec.

--- src/parse-price.spec.ts
+++ src/parse-price.spec.ts
@@ -1,17 +1,26 @@
+
+interface Price {
+    coefficient: number;
+    exponent: number;
+    currency: string;
+}
+
 /**
  * @deprecated wip
  * @param priceString
  */
-function parsePrice(priceString: string) {
+function parsePrice(priceString: string): Price {
     throw new Error('😱 Not implemented yet!');
 }
 
 describe('parsePrice', () => {
 
-    xit('should parse price', () => {
+    xit('should parse price without currency', () => {
 
         expect(parsePrice('12.12')).toEqual({
-
+            coefficient: 1212,
+            exponent: -2,
+            currency: null
         });
 
     });
