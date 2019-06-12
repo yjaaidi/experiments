5e9932641e9cc9415be924255410722fea24132d
Younes JAAIDI <yjaaidi@gmail.com>
2019-06-12 16:54:51 +0300

    🚧 add items to cart.

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -1,12 +1,37 @@
+class Cart {
+    /**
+     * @deprecated 🚧 Work in progress.
+     */
+    addItem(item: Item) {
+        throw new Error('🚧 work in progress!');
+    }
+
+    /**
+     * @deprecated 🚧 Work in progress.
+     */
+    getItemList() {
+        throw new Error('🚧 work in progress!');
+    }
+}
+
+/**
+ * @deprecated 🚧 Work in progress.
+ */
+class Item {
+    constructor(name: string, price: number) {
+        throw new Error('🚧 work in progress!');
+    }
+}
+
 describe('Cart', () => {
     xit('🚧 should add items', () => {
-        // const cart = new Cart();
-        // const hummus = new Item('Hummus', 5);
-        // const shaskshouka = new Item('Shakshouka', 10);
-        // cart.addItem(hummus);
-        // cart.addItem(shaskshouka);
-        // const itemList = cart.getItemList();
-        // expect(itemList).toEqual([hummus, shaskshouka]);
+        const cart = new Cart();
+        const hummus = new Item('Hummus', 5);
+        const shaskshouka = new Item('Shakshouka', 10);
+        cart.addItem(hummus);
+        cart.addItem(shaskshouka);
+        const itemList = cart.getItemList();
+        expect(itemList).toEqual([hummus, shaskshouka]);
     });
 
     it.todo('should get total price');
