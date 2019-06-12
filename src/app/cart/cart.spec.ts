29744e44c1ce2d66deaf350a27b505b74173aab3
Younes JAAIDI <yjaaidi@gmail.com>
2019-06-12 16:51:10 +0300

    🚧 add items to cart.

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -1,5 +1,10 @@
 describe('Cart', () => {
-    it.todo('should add items');
+    xit('should add items', () => {
+        // create a cart
+        // create an item called hummus with a price of 5
+        // create an item called shakshouka
+        throw new Error('🚧 work in progress!');
+    });
 
     it.todo('should get total price');
 });
