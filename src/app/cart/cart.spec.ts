d896d7ac7dc6b6d4dcceaeb891e3c8a8dcccc8f3
Younes JAAIDI <yjaaidi@gmail.com>
2019-06-12 16:51:41 +0300

    🚧 add items to cart.

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -3,6 +3,8 @@ describe('Cart', () => {
         // create a cart
         // create an item called hummus with a price of 5
         // create an item called shakshouka
+        // get items from cart
+        // check the content
         throw new Error('🚧 work in progress!');
     });
 
