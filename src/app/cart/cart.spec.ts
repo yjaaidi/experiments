c0345bf4ce3777ae2336f7431337074a1d87fe88
Younes JAAIDI <yjaaidi@gmail.com>
2019-06-12 16:53:57 +0300

    🚧 add items to cart.

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -1,11 +1,12 @@
 describe('Cart', () => {
     xit('🚧 should add items', () => {
-        const cart = new Cart();
-        const hummus = new Item('Hummus', 5);
-        const shaskshouka = new Item('Shakshouka', 10);
-        // @todo add items
-        const itemList = cart.getItemList();
-        expect(itemList).toEqual([hummus, shaskshouka]);
+        // const cart = new Cart();
+        // const hummus = new Item('Hummus', 5);
+        // const shaskshouka = new Item('Shakshouka', 10);
+        // cart.addItem(hummus);
+        // cart.addItem(shaskshouka);
+        // const itemList = cart.getItemList();
+        // expect(itemList).toEqual([hummus, shaskshouka]);
     });
 
     it.todo('should get total price');
