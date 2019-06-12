24cedc9c4b4eb40782f7210f299bc90bad0c8ec0
Younes JAAIDI <yjaaidi@gmail.com>
2019-06-12 16:52:36 +0300

    🚧 add items to cart.

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -1,5 +1,9 @@
 describe('Cart', () => {
     xit('🚧 should add items', () => {
+        // const cart = new Cart();
+        // const hummus = new Item('Hummus', 5);
+        // const shaskshouka = new Item('Shakshouka', 10);
+
         // create a cart
         // create an item called hummus with a price of 5
         // create an item called shakshouka
