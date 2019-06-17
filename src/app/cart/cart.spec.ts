5db46e5616492d77b6846e7037bee778b91c9712
Younes JAAIDI <yjaaidi@gmail.com>
2019-06-17 20:04:22 +0200

    🚧 User should add items to cart.

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -1,30 +1,21 @@
 class Cart {
     private _itemList: Item[] = [];
 
-    /**
-     * @deprecated 🚧 Work in progress.
-     */
     addItem(item: Item) {
         this._itemList = [...this._itemList, item];
     }
 
-    /**
-     * @deprecated 🚧 Work in progress.
-     */
     getItemList() {
         return this._itemList;
     }
 }
 
-/**
- * @deprecated 🚧 Work in progress.
- */
 class Item {
     constructor(public name: string, public price: number) {}
 }
 
 describe('Cart', () => {
-    it('🚧 should add items', () => {
+    it('should add items', () => {
         const cart = new Cart();
         const burger = new Item('Burger', 12);
         const butter = new Item('Double Beurre', 5);
