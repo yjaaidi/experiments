ecd8f42ed8451167597cd1a6b4cc21f3de78d470
Younes JAAIDI <yjaaidi@gmail.com>
2019-06-17 20:02:56 +0200

    🚧 User should add items to cart.

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -18,20 +18,17 @@ class Cart {
  * @deprecated 🚧 Work in progress.
  */
 class Item {
-    constructor(name: string, price: number) {
-        throw new Error('🚧 work in progress!');
-    }
+    constructor(public name: string, public price: number) {}
 }
 
 describe('Cart', () => {
-    xit('🚧 should add items', () => {
+    it('🚧 should add items', () => {
         const cart = new Cart();
         const burger = new Item('Burger', 12);
         const butter = new Item('Double Beurre', 5);
-        cart.addItem(burger);
-        cart.addItem(butter);
-        const itemList = cart.getItemList();
-        expect(itemList).toEqual([burger, butter]);
-        throw new Error('🚧 work in progress!');
+        // cart.addItem(burger);
+        // cart.addItem(butter);
+        // const itemList = cart.getItemList();
+        // expect(itemList).toEqual([burger, butter]);
     });
 });
