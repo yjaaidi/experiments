78d78da5e32d029be7ab0b04089c6b8011f527fb
Younes JAAIDI <yjaaidi@gmail.com>
2019-06-17 19:54:23 +0200

    🚧 User should add items to cart.

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -1,6 +1,11 @@
 describe('Cart', () => {
     xit('🚧 should add items', () => {
         // create new cart
+        // create new item: sandwich burger 12
+        // create new item: double beurre 5
+        // add items to cart with cart.addItem
+        // get item list
+        // check items
         throw new Error('🚧 work in progress!');
     });
 });
