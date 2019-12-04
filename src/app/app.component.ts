dbd40cda19d2e7c7fe6077e8984a990653da9eac
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-04 01:29:18 +0100

    🚧 benchmark threejs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { CubeComponent } from './cube/cube.component';
-import { Component, ViewChild, ElementRef, OnInit, ViewChildren } from '@angular/core';
-import { DirectionalLight, DirectionalLightHelper, Mesh, MeshBasicMaterial, BoxGeometry, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
-
-export const randomAngle = () => Math.random() * 360;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'three-benchmark';
-
-  @ViewChild('scene', {static: true})
-  sceneEl: ElementRef;
-
-  @ViewChildren(CubeComponent) 
-  cubeList: CubeComponent[];
-
-  ngOnInit() {
-
-  }
-
+export class AppComponent {
 }
 
