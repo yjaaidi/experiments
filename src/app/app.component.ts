ebf5b5e6bcde877b4a73b55269ad042d196bae50
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-04 01:27:03 +0100

    🚧 benchmark threejs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { CubeComponent } from './cube/cube.component';
+import { Component, ViewChild, ElementRef, OnInit, ViewChildren } from '@angular/core';
 import { DirectionalLight, DirectionalLightHelper, Mesh, MeshBasicMaterial, BoxGeometry, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 
 export const randomAngle = () => Math.random() * 360;
@@ -14,42 +15,10 @@ export class AppComponent implements OnInit {
   @ViewChild('scene', {static: true})
   sceneEl: ElementRef;
 
+  @ViewChildren(CubeComponent) 
+  cubeList: CubeComponent[];
+
   ngOnInit() {
-    const scene = new Scene();
-    const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-    const renderer = new WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    this.sceneEl.nativeElement.appendChild( renderer.domElement );
-
-    const count = 1000;
-    const boxes = Array(count).fill(null).map(() => [1, 1, 1]);
-    
-    const cubes = boxes.map(box => new Mesh(new BoxGeometry(...box), new MeshBasicMaterial( { color: 0xffffff * Math.random() } )));
-    cubes.forEach(_cube => {
-      _cube.rotation.x = Math.random() * 360;
-      _cube.rotation.y = Math.random() * 360;
-      _cube.rotation.z = Math.random() * 360;
-      scene.add(_cube);
-    });
-
-    camera.position.x = 2;
-    camera.position.z = 2;
-    camera.rotation.y = .8;
-
-    const animate = () => {
-      requestAnimationFrame( animate );
-
-      cubes.forEach(cube => {
-        cube.rotation.x += 0.01;
-        // cube.rotation.z += 0.01;
-      })
-
-      renderer.render( scene, camera );
-    };
-
-    animate();
-
 
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CubeComponent } from './cube/cube.component';
+import { SceneComponent } from './scene/scene.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CubeComponent,
+    SceneComponent
   ],
   imports: [
     BrowserModule
