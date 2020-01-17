2433c2b9e9c17c7565d2807f8c6ed54d1372ff1c
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-17 18:21:25 +0100

    🚧 search files

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,12 +1,14 @@
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatFormFieldModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
