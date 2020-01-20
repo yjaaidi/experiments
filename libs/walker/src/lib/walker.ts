f388de6ce38db3a346e12f23d1ae22e729e2e764
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-20 15:38:59 +0100

    🚧 use mongodb

--- libs/walker/src/lib/walker.ts
+++ libs/walker/src/lib/walker.ts
@@ -1,3 +1,36 @@
-export function walker(): string {
-  return 'walker';
+import { Line } from '@demo/api-interfaces';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { createInterface } from 'readline';
+import { Observable } from 'rxjs';
+import { walk } from 'walk';
+
+export function getFiles(path: string): Observable<string> {
+  return new Observable<string>(observer => {
+    const walker = walk(path);
+    walker.on('file', (directorypath, fileStats, next) => {
+      observer.next(join(directorypath, fileStats.name));
+      next();
+    });
+    walker.on('end', () => observer.complete());
+    return () => walker.pause();
+  });
+}
+
+export function readLines(filePath: string): Observable<Line> {
+  return new Observable<Line>(observer => {
+    const reader = createInterface({
+      input: createReadStream(filePath)
+    });
+    let lineNumber = 1;
+    reader.on('line', content =>
+      observer.next({
+        content,
+        file: filePath,
+        number: lineNumber++
+      })
+    );
+    reader.on('close', () => observer.complete());
+    return () => reader.close();
+  });
 }
