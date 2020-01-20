00ef252d17c62b60e610b08ef77edf0b1b0125cc
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-20 14:14:45 +0100

    🚧 add stats app

--- apps/stats/src/app/app.component.ts
+++ apps/stats/src/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+export interface Stat {
+  date: Date;
+  cpuUsage: number;
+  memoryUsage: number;
+}
 
 @Component({
   selector: 'demo-root',
@@ -10,6 +18,25 @@ import gql from 'graphql-tag';
 })
 export class AppComponent implements OnInit {
   title = 'stats';
+
+  private _stat$: Observable<Stat> = this._apollo
+    .subscribe({
+      query: gql`
+        subscription {
+          stat {
+            date
+            cpuUsage
+            memoryUsage
+          }
+        }
+      `
+    })
+    .pipe(
+      map(({ data }: { data: any }) => ({
+        date: new Date(data.stat.date),
+        ...data.stat
+      }))
+    );
 
   private _statsSubscription = gql`
     subscription {
@@ -24,12 +51,8 @@ export class AppComponent implements OnInit {
   constructor(private _apollo: Apollo) {}
 
   ngOnInit() {
-    this._apollo
-      .subscribe({
-        query: this._statsSubscription
-      })
-      .subscribe(({ data }) => {
-        console.log(data);
-      });
+    this._stat$.subscribe(stat => {
+      console.log(stat);
+    });
   }
 }
