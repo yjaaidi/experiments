f267dff9b0d0eb9c175701edc6e8f4d35cc8571e
Younes Jaaidi <yjaaidi@gmail.com>
2020-01-22 11:57:40 +0100

    🛠 skip unsubscribe interceptor if not http

--- apps/api/src/app/unsubscribe-on-close.interceptor.ts
+++ apps/api/src/app/unsubscribe-on-close.interceptor.ts
@@ -1,21 +1,18 @@
-import {
-  CallHandler,
-  ExecutionContext,
-  Injectable,
-  NestInterceptor
-} from '@nestjs/common';
-import { bindCallback, Observable } from 'rxjs';
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { IncomingMessage } from 'http';
+import { fromEvent, Observable } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 @Injectable()
 export class UnsubscribeOnCloseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const [request] = context.getArgs();
+    if (context.getType() !== 'http') {
+      return next.handle();
+    }
 
-    const close$ = bindCallback(request.socket.on).call(
-      request.socket,
-      'close'
-    );
+    const request = context.getArgByIndex<IncomingMessage>(0);
+
+    const close$ = fromEvent(request.socket, 'close');
 
     return next.handle().pipe(takeUntil(close$));
   }
