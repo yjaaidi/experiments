33d3a57262bdc55123819a052164c4fc56c92626
Younes Jaaidi <yjaaidi@gmail.com>
2020-11-22 17:10:32 +0100

    🚧 add CypressHarnessEnvironment

---
+++ apps/demo-e2e/src/integration/demo.spec.ts
@@ -0,0 +1,130 @@
+import {
+  ElementDimensions,
+  HarnessEnvironment,
+  ModifierKeys,
+  TestElement,
+  TestKey,
+  TextOptions,
+} from '@angular/cdk/testing';
+import { MatDatepickerInputHarness } from '@angular/material/datepicker/testing';
+
+export class CypressElement implements TestElement {
+  constructor(readonly element: JQuery<HTMLElement>) {}
+
+  blur(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  clear(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  click(): Promise<void>;
+  click(location: 'center'): Promise<void>;
+  click(relativeX: number, relativeY: number): Promise<void>;
+  click(relativeX?: any, relativeY?: any): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  rightClick?(relativeX: number, relativeY: number): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  focus(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  getCssValue(property: string): Promise<string> {
+    throw new Error('Method not implemented.');
+  }
+  hover(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  mouseAway(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  sendKeys(...keys: (string | TestKey)[]): Promise<void>;
+  sendKeys(
+    modifiers: ModifierKeys,
+    ...keys: (string | TestKey)[]
+  ): Promise<void>;
+  sendKeys(modifiers?: any, ...keys: any[]): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  text(options?: TextOptions): Promise<string> {
+    throw new Error('Method not implemented.');
+  }
+  getAttribute(name: string): Promise<string> {
+    throw new Error('Method not implemented.');
+  }
+  hasClass(name: string): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+  getDimensions(): Promise<ElementDimensions> {
+    throw new Error('Method not implemented.');
+  }
+  getProperty(name: string): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+  matchesSelector(selector: string): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+  isFocused(): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+  setInputValue?(value: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  selectOptions?(...optionIndexes: number[]): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  dispatchEvent?(name: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class CypressHarnessEnvironment extends HarnessEnvironment<
+  JQuery<HTMLElement>
+> {
+  static loader(): Cypress.Chainable<CypressHarnessEnvironment> {
+    return cy.get('body').pipe((body) => new CypressHarnessEnvironment(body));
+  }
+
+  forceStabilize(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  waitForTasksOutsideAngular(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  protected getDocumentRoot(): JQuery<HTMLElement> {
+    throw new Error('Method not implemented.');
+  }
+  protected createTestElement(element: JQuery<HTMLElement>): TestElement {
+    throw new Error('Method not implemented.');
+  }
+  protected createEnvironment(
+    element: JQuery<HTMLElement>
+  ): HarnessEnvironment<JQuery<HTMLElement>> {
+    throw new Error('Method not implemented.');
+  }
+  protected getAllRawElements(
+    selector: string
+  ): Promise<JQuery<HTMLElement>[]> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+describe('demo', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should select date', () => {
+    const loader = CypressHarnessEnvironment.loader();
+    loader
+      .pipe(
+        async (loader) => {
+          const harness = await loader.getHarness(MatDatepickerInputHarness);
+          return harness.isCalendarOpen();
+        },
+        {
+          timeout: 10,
+        }
+      )
+      .should('be.false');
+  });
+});
