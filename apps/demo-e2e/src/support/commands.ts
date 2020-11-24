d33ae4a56a44aea56b3183a96e2c6e7c84fe6918
Younes Jaaidi <yjaaidi@gmail.com>
2020-11-24 20:24:52 +0100

    🐞 fix harness predicate

--- apps/demo-e2e/src/support/commands.ts
+++ apps/demo-e2e/src/support/commands.ts
@@ -10,7 +10,11 @@
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 
-import { ComponentHarness, HarnessQuery } from '@angular/cdk/testing';
+import {
+  ComponentHarness,
+  HarnessPredicate,
+  HarnessQuery,
+} from '@angular/cdk/testing';
 import 'cypress-pipe';
 import { CypressHarnessEnvironment } from './cypress-harness';
 
@@ -29,6 +33,15 @@ declare global {
 Cypress.Commands.add(
   'harness',
   <T extends ComponentHarness>(query: HarnessQuery<T>) => {
+    /* @hack recreate `HarnessPredicate` because the one referenced in tests
+     * is not the same as here due to the way Cypress builds the tests
+     * Otherwise, the `isinstanceof` call here is broken: https://github.com/angular/components/blob/5bfe312b5ee247109ed5d35cc07660fca04db0fe/src/cdk/testing/harness-environment.ts#L220. */
+    if ('harnessType' in query) {
+      const originalQuery = query;
+      query = new HarnessPredicate(originalQuery.harnessType, {});
+      Object.assign(query, originalQuery);
+    }
+
     /* Create a local variable so `pipe` can log name. */
     const getHarness = (body) =>
       new CypressHarnessEnvironment(body).getHarness(query);
