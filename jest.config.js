e0c8c11e18b08ca4e290a1d453864f3e256672ea
Younes Jaaidi <yjaaidi@gmail.com>
2020-11-25 13:49:52 +0100

    ✅ add lib

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/app'],
+  projects: ['<rootDir>/apps/app', '<rootDir>/libs/hello'],
 };
---
+++ libs/hello/src/lib/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Hello!</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'HelloWorld'
+});
+</script>
