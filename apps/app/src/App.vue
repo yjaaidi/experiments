6b4843019c8a4252647ae6176e1b2ca41223a81c
Younes Jaaidi <yjaaidi@gmail.com>
2020-11-25 14:21:47 +0100

    ✅ add publishable lib

--- apps/app/src/App.vue
+++ apps/app/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { HelloWorld } from '@vue-demo/hello';
 
 export default Vue.extend({
   name: 'App',
