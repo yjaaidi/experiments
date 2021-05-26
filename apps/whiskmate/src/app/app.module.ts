d220f0d946e0ebd5ae56391e897c7a4d593273e7
Younes Jaaidi <yjaaidi@gmail.com>
2021-05-26 23:04:57 +0200

    🛠 setup angular materials
    ng add @angular/material

--- apps/whiskmate/src/app/app.module.ts
+++ apps/whiskmate/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MealSearchModule],
+  imports: [BrowserAnimationsModule, BrowserModule, MealSearchModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
