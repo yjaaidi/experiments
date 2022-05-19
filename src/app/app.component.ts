779c07045380e4d8b5bdcc8cfa8f1243e04ef503
Younes Jaaidi <yjaaidi@gmail.com>
2022-05-19 18:31:14 +0200

    feat: ✨ add simple reactivity based on RxState + inject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,33 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { injectState } from './inject-state';
 
 @Component({
-  
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'mc-counter',
+  template: `<div>{{ state.counter }}</div>
+    <div>
+      <button (click)="state.counter = 0">RESET</button>
+      <button (click)="increment()">+</button>
+    </div>`,
 })
+export class CounterComponent {
+  state = injectState({
+    counter: 0,
+  });
 
-@Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
-})
-export class AppComponent {
-  title = 'demo';
+  increment() {
+    /* Adding a dirty setTimeout to show that reactivity works anyway. */
+    setTimeout(() => {
+      this.state.counter += 1;
+    }, 500);
+  }
 }
+
+@Component({
+  standalone: true,
+  selector: 'mc-root',
+  imports: [CounterComponent],
+  template: `<mc-counter></mc-counter>`,
+})
+export class AppComponent {}
