5065667fc01fb71c5df269253c266f9a5c2451cf
Younes Jaaidi <yjaaidi@gmail.com>
2022-09-30 11:50:03 +0200

    refactor: 🛠 add @angular/material
    yarn add -D @angular/material
    nx g @angular/material:ng-add

--- apps/mr-button/src/app/app.module.ts
+++ apps/mr-button/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
