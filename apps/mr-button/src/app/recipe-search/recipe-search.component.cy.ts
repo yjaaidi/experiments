a4767c5cf93f1042f4b55dd7397c9747b1cae591
Younes Jaaidi <yjaaidi@gmail.com>
2022-10-03 14:12:11 +0200

    test: ✅ add empty spec

---
+++ apps/mr-button/src/app/recipe-search/recipe-search.component.cy.ts
@@ -0,0 +1,6 @@
+import { RecipeSearchComponent } from './recipe-search.component';
+describe(RecipeSearchComponent.name, () => {
+  it('🚧 should add recipe to meal planner when clicked', () => {
+    throw new Error('🚧 Work in progress!');
+  })
+})
