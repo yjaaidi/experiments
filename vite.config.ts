a1d017301bd521bade46f366cc3ecc30339d5666
Younes Jaaidi <yjaaidi@gmail.com>
2022-11-23 09:25:40 +0100

    docs: 📝 explain why we are using resolve.conditions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   resolve: {
+    /* @angular/material is using "style" as a Custom Conditional Cxport to expose prebuilt styles etc... */
     conditions: ['style'],
   },
 });
