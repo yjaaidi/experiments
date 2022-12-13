ab682d67ec097ae4c9c9efa030c0dd41a86222eb
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-13 17:14:49 +0100

    test: ✅ let playwright start the server when needed

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,14 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  webServer: process.env.BASE_URL
+    ? undefined
+    : {
+        command: 'yarn start',
+        url: 'http://127.0.0.1:3000/recipes',
+        timeout: 20 * 1000,
+        reuseExistingServer: !process.env.CI,
+      },
   use: {
     baseURL: process.env.BASE_URL ?? 'http://127.0.0.1:3000',
   },
