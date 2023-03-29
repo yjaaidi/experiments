f281302b88d93493a99b220998ad69a30b14412f
Younes Jaaidi <yjaaidi@gmail.com>
2023-03-30 01:02:25 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'], 
+  imports: [CommonModule]
+})
+export class AppComponent {
+  title = 'demo';
+}
