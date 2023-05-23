4b9f45732141bfb6d7db08557004108e008d36dc
Younes Jaaidi <yjaaidi@gmail.com>
2023-05-24 00:02:43 +0200

    feat: ✨ add link to home

--- src/app/shared/toolbar.component.ts
+++ src/app/shared/toolbar.component.ts
@@ -1,21 +1,28 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ToolbarChipComponent } from './toolbar-chip.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'wm-toolbar',
-  imports: [MatToolbarModule, ToolbarChipComponent],
+  imports: [MatToolbarModule, ToolbarChipComponent, RouterLink],
   template: `
     <mat-toolbar color="primary">
-      <h1>{{ title }}</h1>
+      <a class="link" routerLink="/"
+        ><h1>{{ title }}</h1></a
+      >
       <div class="spacer"></div>
       <wm-toolbar-chip>🛒</wm-toolbar-chip>
     </mat-toolbar>
   `,
   styles: [
     `
+      .link {
+        color: white;
+        text-decoration: none;
+      }
       .spacer {
         flex: 1 1 auto;
       }
