b29d1a7ca18150be9bf416065269db21a56f8b4d
Younes Jaaidi <yjaaidi@gmail.com>
2023-08-19 17:49:13 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'demo';
+}
