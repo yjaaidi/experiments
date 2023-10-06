8f5853d218de2d666deea0e29cb05798931fe264
Younes Jaaidi <yjaaidi@gmail.com>
2023-10-06 16:48:01 +0200

    feat: ✨ clean up the page

--- apps/whiskmate/src/app/app.component.ts
+++ apps/whiskmate/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'whiskmate-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet],
+  selector: 'wm-app',
+  template: `
+    <h1>Hello!</h1>
+    <router-outlet />
+  `,
 })
-export class AppComponent {
-  title = 'whiskmate';
-}
+export class AppComponent {}
