8dfd253a6de6303c8fcd9415b01c9bf313b37bc4
Younes Jaaidi <yjaaidi@gmail.com>
2023-10-11 17:33:50 +0200

    refactor: 🛠 we don't need the country hack for following demos

--- libs/recipe/suggestions-feature/src/lib/recipe-suggestions.component.ts
+++ libs/recipe/suggestions-feature/src/lib/recipe-suggestions.component.ts
@@ -1,17 +1,6 @@
 import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-
-import { ActivatedRoute } from '@angular/router';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { catchError, defer, of } from 'rxjs';
-import { rxComputed } from '@jscutlery/rx-computed';
-import { pending, suspensify } from '@jscutlery/operators';
 import {
   GridComponent,
   MessageComponent,
@@ -37,13 +26,9 @@ import { RecipeAddButtonComponent } from '@whiskmate/meal-planner/recipe-add-but
   template: `
     <h2>Suggestions d'Ottolenghi</h2>
 
-    <wm-message *ngIf="suggestionsSuspense().hasError">
-      💥 Something went wrong
-    </wm-message>
-
     <wm-grid>
       <wm-recipe-preview
-        *ngFor="let recipe of suggestions(); trackBy: trackById"
+        *ngFor="let recipe of recipes; trackBy: trackById"
         [recipe]="recipe"
       >
         <wm-recipe-add-button [recipe]="recipe" />
@@ -59,54 +44,26 @@ import { RecipeAddButtonComponent } from '@whiskmate/meal-planner/recipe-add-but
   ],
 })
 export class RecipeSuggestionsComponent {
-  suggestionsSuspense = rxComputed(
-    () =>
-      this._getSuggestions().pipe(
-        catchError((error) => {
-          console.error(error);
-          throw error;
-        }),
-        suspensify()
-      ),
-    { initialValue: pending }
-  );
-  suggestions = () => {
-    const suspense = this.suggestionsSuspense();
-    return suspense.hasValue ? suspense.value : [];
-  };
+  recipes = [
+    {
+      id: 'rec_babaganoush',
+      name: 'Babaganoush by Ottolenghi',
+      description: 'A delicious Babaganoush.',
+      ingredients: [],
+      pictureUri:
+        'https://ottolenghi.co.uk/pub/media/contentmanager/content/cache/646x458//Baba-Ganoush-1.jpg',
+      steps: [],
+    },
+    {
+      id: 'rec_beef',
+      name: 'Beef with Garlic by Ottolenghi',
+      description: 'A delicious Beef with Garlic.',
+      pictureUri:
+        'https://ottolenghi.co.uk/pub/media/contentmanager/content/cache/646x458//Barbecue-beef-short-ribs-with-black-garlic-and-urfa-chilli.jpg',
+      ingredients: [],
+      steps: [],
+    },
+  ];
 
   trackById = trackById;
-
-  private _route = inject(ActivatedRoute);
-  private _queryParamMap = toSignal(this._route.queryParamMap);
-  private _country = computed(() => this._queryParamMap()?.get('country'));
-
-  private _getSuggestions() {
-    return defer(() => {
-      if (this._country() === 'fr') {
-        return of([
-          {
-            id: 'rec_babaganoush',
-            name: 'Babaganoush by Ottolenghi',
-            description: 'A delicious Babaganoush.',
-            ingredients: [],
-            pictureUri:
-              'https://ottolenghi.co.uk/pub/media/contentmanager/content/cache/646x458//Baba-Ganoush-1.jpg',
-            steps: [],
-          },
-          {
-            id: 'rec_beef',
-            name: 'Beef with Garlic by Ottolenghi',
-            description: 'A delicious Beef with Garlic.',
-            pictureUri:
-              'https://ottolenghi.co.uk/pub/media/contentmanager/content/cache/646x458//Barbecue-beef-short-ribs-with-black-garlic-and-urfa-chilli.jpg',
-            ingredients: [],
-            steps: [],
-          },
-        ]);
-      } else {
-        throw new Error('unknown country');
-      }
-    });
-  }
 }
