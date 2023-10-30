0769d58dd7b4da52d15d4c4da6bddb286d74726d
Younes Jaaidi <yjaaidi@gmail.com>
2023-10-30 22:24:14 +0100

    build: 📦 set up @angular/material
    pnpm i @angular/material@next
    nx g @angular/material:ng-add --project whiskmate

--- apps/whiskmate/src/app/app.config.ts
+++ apps/whiskmate/src/app/app.config.ts
@@ -4,7 +4,8 @@ import {
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), provideAnimations()],
 };
