9196c0a6d2b7857069f29411fec4b5516d3bc7ea
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-01 10:03:32 +0100

    test: ✅ fix jest typing issue

--- apps/whiskmate/src/test-setup.ts
+++ apps/whiskmate/src/test-setup.ts
@@ -1,3 +1,6 @@
+/* @hack fix https://youtrack.jetbrains.com/issue/WEB-62299/types-are-not-properly-resolved-when-using-pnpm */
+/// <reference types="@types/jest" />
+
 // @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
 globalThis.ngJest = {
   testEnvironmentOptions: {
