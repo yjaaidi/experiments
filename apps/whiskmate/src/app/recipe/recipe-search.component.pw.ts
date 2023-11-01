618783d6a0f30d08ef351a7a00b02863ef45110c
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-01 12:06:14 +0100

    test: ✅ set up playwright starter

---
+++ apps/whiskmate/src/app/recipe/recipe-search.component.pw.ts
@@ -0,0 +1,33 @@
+import { expect, test } from '@jscutlery/playwright-ct-angular';
+import { RecipeSearchComponent } from './recipe-search.component';
+
+test('should search recipes without filtering', async ({ mount }) => {
+  const locator = await mount(RecipeSearchComponent);
+
+  await expect(locator.getByRole('heading', { level: 2 })).toHaveText([
+    'Burger',
+    'Salad',
+  ]);
+});
+
+test('should filter recipes by keyword', async ({ mount }) => {
+  const locator = await mount(RecipeSearchComponent);
+
+  await locator.getByLabel('Keywords').fill('Bur');
+
+  await expect(locator.getByRole('heading', { level: 2 })).toHaveText([
+    'Burger',
+  ]);
+});
+
+test('should show "no results" messagee when no recipes match', async ({
+  mount,
+}) => {
+  const locator = await mount(RecipeSearchComponent);
+
+  await locator.getByLabel('Keywords').fill('arecipethatdoesnotexist');
+
+  await expect(locator).toContainText('no results', {
+    ignoreCase: true,
+  });
+});
