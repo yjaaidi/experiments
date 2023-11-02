acc09026b38af510808a271e23d5b044ace1ca4d
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-02 16:32:58 +0100

    test: ✅ switch to spying stub for demo purposes

--- apps/whiskmate/src/app/recipe/recipe-search.component.spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.component.spec.ts
@@ -1,55 +1,64 @@
 import { render, screen } from '@testing-library/angular';
 import { RecipeSearchComponent } from './recipe-search.component';
 import { userEvent } from '@testing-library/user-event';
-import { recipeMother } from '../testing/recipe.mother';
 import {
-  provideRecipeRepositoryFake,
-  RecipeRepositoryFake,
-} from './recipe-repository.fake';
+  RecipeRepository,
+  RecipeRepositoryDef,
+} from './recipe-repository.service';
+import { of } from 'rxjs';
+import { recipeMother } from '../testing/recipe.mother';
 
 jest.useFakeTimers();
 
 describe(RecipeSearchComponent.name, () => {
   it('should search recipes without filtering', async () => {
-    const { getRecipeNames } = await renderComponent();
+    const { getRecipeNames, repo } = await renderComponent();
 
     expect(getRecipeNames()).toEqual(['Burger', 'Salad']);
+    expect(repo.search).toBeCalledTimes(1);
+    expect(repo.search).toBeCalledWith(undefined);
   });
 
   it('should filter recipes using keywords', async () => {
-    const { getRecipeNames, typeKeywords } = await renderComponent();
+    const { getRecipeNames, typeKeywords, repo } = await renderComponent();
+
+    repo.search.mockClear();
+    repo.search.mockReturnValue(
+      of([recipeMother.withBasicInfo('Burger').build()])
+    );
 
     await typeKeywords('Bur');
 
     expect(getRecipeNames()).toEqual(['Burger']);
+    expect(repo.search).toBeCalledTimes(1);
+    expect(repo.search).toBeCalledWith('Bur');
   });
 
-  it('should show "no results" message when no recipes match', async () => {
-    const { getRecipeNames, typeKeywords } = await renderComponent();
-
-    await typeKeywords('arecipethatdoesnotexist');
-
-    expect(getRecipeNames()).toEqual([]);
-    expect(
-      screen.getByText('no results', {
-        exact: false,
-      })
-    ).toBeVisible();
-  });
+  it.todo('should show "no results" message when no recipes match');
 
   async function renderComponent() {
+    const repo: jest.Mocked<RecipeRepositoryDef> = {
+      search: jest.fn(),
+    };
+
+    repo.search.mockReturnValue(
+      of([
+        recipeMother.withBasicInfo('Burger').build(),
+        recipeMother.withBasicInfo('Salad').build(),
+      ])
+    );
+
     const { detectChanges } = await render(RecipeSearchComponent, {
-      configureTestBed: (testBed) =>
-        testBed
-          .inject(RecipeRepositoryFake)
-          .setRecipes([
-            recipeMother.withBasicInfo('Burger').build(),
-            recipeMother.withBasicInfo('Salad').build(),
-          ]),
-      providers: [provideRecipeRepositoryFake()],
+      providers: [
+        {
+          provide: RecipeRepository,
+          useValue: repo,
+        },
+      ],
     });
 
     return {
+      repo,
       getRecipeNames() {
         return screen
           .queryAllByRole('heading', { level: 2 })
