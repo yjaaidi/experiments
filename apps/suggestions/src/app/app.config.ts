50fa51b4e1e3fafa13f1d47a30fed3c7a35af20e
Younes Jaaidi <yjaaidi@gmail.com>
2023-12-01 03:06:08 +0100

    refactor: 🛠 format files

--- apps/suggestions/src/app/app.config.ts
+++ apps/suggestions/src/app/app.config.ts
@@ -7,5 +7,8 @@ import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), provideAnimations()],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideAnimations(),
+  ],
 };
