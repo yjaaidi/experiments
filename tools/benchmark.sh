ef01a1079ff22f740a5bcf0087677bcdbe9bde74
Younes Jaaidi <younes@marmicode.io>
2024-03-23 00:09:07 +0100

    test: ✅ add jest swc benchmark

---
+++ tools/benchmark.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+bun jest --clearCache
+
+BENCHMARK_FOLDER=apps/demo/src/app/benchmark
+rm -rf $BENCHMARK_FOLDER
+mkdir -p $BENCHMARK_FOLDER
+for i in $(seq 1 3000); do
+  cat apps/demo/src/app/app.component.spec.ts | sed "s/app.component/app.component.$i/g" > $BENCHMARK_FOLDER/app.component.$i.spec.ts
+  cat apps/demo/src/app/app.component.ts | sed "s/app.component/app.component.$i/g" | sed "s|\./recipe|../recipe|g" > $BENCHMARK_FOLDER/app.component.$i.ts
+  cp apps/demo/src/app/app.component.html $BENCHMARK_FOLDER/app.component.$i.html
+done
+
+nx test demo --skip-nx-cache
