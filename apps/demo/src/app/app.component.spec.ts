32eb2cf066f8f0a18baf041e6c7a5b73428ee634
Younes Jaaidi <younes@marmicode.io>
2024-04-03 23:38:19 +0200

    test: ✅ add more tests
    this will make the total result more realistic
    and make the speed more palpable

--- apps/demo/src/app/app.component.spec.ts
+++ apps/demo/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { AppComponent } from './app.component';
 import { TestBed } from '@angular/core/testing';
 
-test('ok', () => {
+test.each(Array.from(Array(10).keys()))('ok %i', () => {
   const fixture = TestBed.createComponent(AppComponent);
   fixture.autoDetectChanges();
 
