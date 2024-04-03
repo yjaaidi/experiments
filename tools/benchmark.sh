90030a79a26640303508341d9e98233e2de50698
Younes Jaaidi <younes@marmicode.io>
2024-04-04 00:02:43 +0200

    test: ✅ speed up result by running less tests

--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -15,7 +15,7 @@ rm -Rf dist node_modules/.vite
 BENCHMARK_FOLDER=apps/demo/src/app/benchmark
 rm -rf $BENCHMARK_FOLDER
 mkdir -p $BENCHMARK_FOLDER
-for i in $(seq 1 500); do
+for i in $(seq 1 200); do
   cat apps/demo/src/app/app.component.spec.ts | sed "s/app.component/app.component.$i/g" > $BENCHMARK_FOLDER/app.component.$i.spec.ts
   cat apps/demo/src/app/app.component.ts | sed "s/app.component/app.component.$i/g" | sed "s|\./recipe|../recipe|g" > $BENCHMARK_FOLDER/app.component.$i.ts
   cp apps/demo/src/app/app.component.html $BENCHMARK_FOLDER/app.component.$i.html
