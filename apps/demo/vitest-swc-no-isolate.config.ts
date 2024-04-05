f80569871e7bbf001be79fe41d15731cb25a76ad
Younes Jaaidi <younes@marmicode.io>
2024-04-05 23:54:53 +0200

    refactor: 🛠️ remove useless configs

--- apps/demo/vitest-swc-no-isolate.config.ts
+++
@@ -1,8 +0,0 @@
-import { mergeConfig } from 'vitest/config';
-import vitestSwcConfig from './vitest-swc.config';
-
-export default mergeConfig(vitestSwcConfig, {
-  test: {
-    isolate: false,
-  },
-});
