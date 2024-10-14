29fad151fd1367cdd59924992758780b6b42a75e
Younes Jaaidi <younes@marmicode.io>
2024-10-14 20:47:35 +0200

    refactor: tidy up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,30 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-import { test as baseTest, expect } from '@test-bear/playwright';
+import { test, expect } from '../testing/test-bear';
 import { AppComponent } from './app.component';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-
-
-const test = baseTest.extend<{ browserStep: Step, step: Step }>({
-  browserStep: async ({page}, use, testInfo) => {
-    await use(async () => {
-      await page.evaluate(async ({testName}) => {
-        const _browserStep = async (fn: () => Promise<void>) => fn();
-        await (globalThis as any)._testBearTests[testName]({browserStep: _browserStep, step: () => {}});
-      }, {testName: testInfo.title});
-    })
-  },
-  step: async ({}, use) => {
-    await use(async (fn) => {
-      await fn();
-    });
-  }
-});
-
-type Step = (fn: () => Promise<void>) => Promise<void>;
-
-test.beforeEach(async ({ page }) => {
-  await page.goto('/');
-});
 
 test('should search recipes without filtering', async ({ page, browserStep, step }) => {
   await browserStep(async () => {
--- src/testing/test-bear.ts
+++ src/testing/test-bear.ts
@@ -1,13 +1,24 @@
-export async function browserStep(fn: () => Promise<void>) {
-  if (isRunningInPlaywright()) {
-    return;
+import { test as baseTest } from '@test-bear/playwright';
+export { expect } from '@test-bear/playwright';
+
+export const test = baseTest.extend<{ browserStep: Step, step: Step }>({
+  browserStep: async ({page}, use, testInfo) => {
+    await use(async () => {
+      await page.evaluate(async ({testName}) => {
+        const _browserStep = async (fn: () => Promise<void>) => fn();
+        await (globalThis as any)._testBearTests[testName]({browserStep: _browserStep, step: () => {}});
+      }, {testName: testInfo.title});
+    })
+  },
+  step: async ({}, use) => {
+    await use(async (fn) => {
+      await fn();
+    });
   }
-  await fn();
-}
+});
 
-function isRunningInPlaywright() {
-  return '_playwrightInstance' in globalThis;
-}
+type Step = (fn: () => Promise<void>) => Promise<void>;
 
-export let _currentTestName: string | undefined;
-export let _currentStepIndex: number | undefined;
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+});
