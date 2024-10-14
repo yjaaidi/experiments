08fec7528290b746dd66a6904d30fd3f339f21bb
Younes Jaaidi <younes@marmicode.io>
2024-10-14 20:53:33 +0200

    refactor: move testbed setup to main.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { test, expect } from '../testing/test-bear';
+import { expect, test } from '../testing/test-bear';
 import { AppComponent } from './app.component';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 test('should search recipes without filtering', async ({ page, browserStep, step }) => {
   await browserStep(async () => {
-    TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
-    const fixture = TestBed.createComponent(AppComponent);
-    await fixture.whenStable();
+    TestBed.createComponent(AppComponent)
   });
 
   await step(async () => {
@@ -15,5 +12,5 @@ test('should search recipes without filtering', async ({ page, browserStep, step
       'Burger',
       'Salad',
     ]);
-  })
+  });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
+import { TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import './app/app.component.spec';
+
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
