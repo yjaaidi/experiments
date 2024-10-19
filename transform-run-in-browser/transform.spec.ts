344248557482bfbeb3f5f1fa7098b669d1dc69f9
Younes Jaaidi <younes@marmicode.io>
2024-10-19 14:58:46 +0200

    test: tidy up

--- transform-run-in-browser/transform.spec.ts
+++ transform-run-in-browser/transform.spec.ts
@@ -3,6 +3,41 @@ import { join } from 'node:path';
 import { expect, test } from 'vitest';
 import transformRunInBrowser from './transform';
 
+test('remove imports used in `runInBrowser` only', () => {
+  const { transform } = setUp();
+  const result = transform(BASIC_TEST);
+  expect
+    .soft(result)
+    .not.toContain(`import { TestBed } from '@angular/core/testing';`);
+  expect
+    .soft(result)
+    .not.toContain(
+      `import { RecipeSearchComponent } from './recipe-search.component';`,
+    );
+});
+
+test('keep imports that are used outside `runInBrowser`', () => {
+  const { transform } = setUp();
+  const result = transform(BASIC_TEST);
+  expect(result).toContain(`import { expect, test } from '@playwright/test';`);
+});
+
+test('replace `runInBrowser` function argument with a function identifier', () => {
+  const { transform } = setUp();
+  const result = transform(BASIC_TEST);
+  expect(result).toMatch(
+    /await runInBrowser\("src_recipe-search.spec.ts-\w+"\)/,
+  );
+});
+
+test.todo(
+  'remove specifiers that are used in `runInBrowser` but keep other imports',
+);
+
+test('extract imports', () => {});
+
+test.todo('extract `runInBrowser` function argument');
+
 const BASIC_TEST = {
   relativeFilePath: 'src/recipe-search.spec.ts',
   code: `
@@ -20,55 +55,27 @@ const BASIC_TEST = {
 `,
 };
 
-test('remove imports used in `runInBrowser` only', () => {
-  const result = transform(BASIC_TEST);
-  expect
-    .soft(result)
-    .not.toContain(`import { TestBed } from '@angular/core/testing';`);
-  expect
-    .soft(result)
-    .not.toContain(
-      `import { RecipeSearchComponent } from './recipe-search.component';`,
-    );
-});
-
-test('keep imports that are used outside `runInBrowser`', () => {
-  const result = transform(BASIC_TEST);
-  expect(result).toContain(`import { expect, test } from '@playwright/test';`);
-});
-
-test('replace `runInBrowser` function argument with a function identifier', () => {
-  const result = transform(BASIC_TEST);
-  expect(result).toMatch(
-    /await runInBrowser\("src_recipe-search.spec.ts-\w+"\)/,
-  );
-});
-
-test.todo(
-  'remove specifiers that are used in `runInBrowser` but keep other imports',
-);
-
-test.todo('extract imports');
-
-test.todo('extract `runInBrowser` function argument');
-
-function transform({
-  relativeFilePath,
-  code,
-}: {
-  relativeFilePath: string;
-  code: string;
-}) {
-  const projectRoot = '/path/to/project';
-  return babelTransform(code, {
-    filename: join(projectRoot, relativeFilePath),
-    plugins: [
-      [
-        transformRunInBrowser,
-        {
-          projectRoot,
-        },
-      ],
-    ],
-  })?.code;
+function setUp() {
+  return {
+    transform({
+      relativeFilePath,
+      code,
+    }: {
+      relativeFilePath: string;
+      code: string;
+    }) {
+      const projectRoot = '/path/to/project';
+      return babelTransform(code, {
+        filename: join(projectRoot, relativeFilePath),
+        plugins: [
+          [
+            transformRunInBrowser,
+            {
+              projectRoot,
+            },
+          ],
+        ],
+      })?.code;
+    },
+  };
 }
