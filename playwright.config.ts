0a3521aa1209a06822f977deab6f2e3aa9c065a6
Younes Jaaidi <younes@marmicode.io>
2024-10-23 11:39:36 +0200

    test: change test port

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default {
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
       /* Base URL to use in actions like `await page.goto('/')`. */
-      baseURL: 'http://localhost:4300',
+      baseURL: 'http://localhost:7357',
 
       /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
       trace: 'on-first-retry',
