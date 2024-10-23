c14d39e46747ed42d983e518d4cc725847176986
Younes Jaaidi <younes@marmicode.io>
2024-10-23 16:12:49 +0200

    build: clear generated folder on reset

--- tools/reset-generated.sh
+++ tools/reset-generated.sh
@@ -5,10 +5,9 @@ set -e
 PROJECT_ROOT=$(dirname $(dirname $(realpath "$0")))
 GENERATED_DIR=$PROJECT_ROOT/playwright/generated
 
+rm -rf $GENERATED_DIR
 mkdir -p $GENERATED_DIR
 
 if [ ! -f $GENERATED_DIR/index.ts ]; then
   echo 'export {};' > $GENERATED_DIR/index.ts
 fi
-
-ng dev -c testing
