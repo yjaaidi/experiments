8c7091caa1b65466827c3e3820a94bcccaecaf61
Younes Jaaidi <younes@marmicode.io>
2024-10-23 23:42:59 +0200

    build: remove useless condition

--- tools/reset-generated.sh
+++ tools/reset-generated.sh
@@ -7,7 +7,4 @@ GENERATED_DIR=$PROJECT_ROOT/playwright/generated
 
 rm -rf $GENERATED_DIR
 mkdir -p $GENERATED_DIR
-
-if [ ! -f $GENERATED_DIR/index.ts ]; then
-  echo 'export {};' > $GENERATED_DIR/index.ts
-fi
+echo 'export {};' > $GENERATED_DIR/index.ts
