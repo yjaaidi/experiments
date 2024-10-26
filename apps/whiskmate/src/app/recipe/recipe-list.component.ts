13fb9731b3db803b694a94a0157925376bef7c23
Younes Jaaidi <younes@marmicode.io>
2024-10-27 01:23:57 +0200

    refactor: 🛠️ tidy up

--- apps/whiskmate/src/app/recipe/recipe-list.component.ts
+++ apps/whiskmate/src/app/recipe/recipe-list.component.ts
@@ -2,13 +2,13 @@ import {
   ChangeDetectionStrategy,
   Component,
   ContentChild,
-  Input,
+  input,
   TemplateRef,
 } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
 import { Recipe } from './recipe';
 import { GridComponent } from '../shared/grid.component';
 import { RecipePreviewComponent } from './recipe-preview.component';
-import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,19 +16,22 @@ import { NgTemplateOutlet } from '@angular/common';
   selector: 'wm-recipe-list',
   template: `
     <wm-grid>
-      @for (recipe of recipes;track recipe.id) {
-      <wm-recipe-preview [recipe]="recipe">
-        <ng-container
-          *ngTemplateOutlet="actionsTemplateRef; context: { $implicit: recipe }"
-        ></ng-container>
-      </wm-recipe-preview>
+      @for (recipe of recipes(); track recipe.id) {
+        <wm-recipe-preview [recipe]="recipe">
+          <ng-container
+            *ngTemplateOutlet="
+              actionsTemplateRef;
+              context: { $implicit: recipe }
+            "
+          ></ng-container>
+        </wm-recipe-preview>
       }
     </wm-grid>
   `,
   imports: [GridComponent, RecipePreviewComponent, NgTemplateOutlet],
 })
 export class RecipeListComponent {
-  @Input({ required: true }) recipes!: Recipe[];
+  recipes = input.required<Recipe[]>();
 
   @ContentChild('actions') actionsTemplateRef!: TemplateRef<{
     $implicit: Recipe;
