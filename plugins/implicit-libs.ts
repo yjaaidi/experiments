fedb6b23a4318a9e8851858c3c54996500230b3b
Younes Jaaidi <younes@marmicode.io>
2024-10-30 16:01:37 +0100

    refactor: 🛠️ tidy up

--- plugins/implicit-libs.ts
+++ plugins/implicit-libs.ts
@@ -1,40 +1,58 @@
 import { CreateNodesV2, ProjectConfiguration } from '@nx/devkit';
-import { dirname, join } from 'node:path';
+import { dirname } from 'path/posix';
 
-export const createNodesV2: CreateNodesV2 = [
-  'libs/*/*/index.ts',
-  (indexFiles) =>
-    indexFiles.map((indexFile) => {
-      const [_, scope, shortName] = indexFile.split('/');
-      const type = shortName.split('-')[0];
-      const projectRoot = dirname(indexFile);
-      const name = `${scope}-${shortName}`;
-
-      return [
-        indexFile,
-        {
-          projects: {
-            [projectRoot]: {
-              projectType: 'library',
-              name,
-              tags: [`scope:${scope}`, `type:${type}`, `name:${name}`],
-              targets: {
-                lint: {
-                  command: 'eslint .',
-                  options: {
-                    cwd: projectRoot,
-                  },
-                },
-                test: {
-                  command: 'vitest',
-                  options: {
-                    cwd: projectRoot,
-                  },
-                },
-              },
-            } satisfies Omit<ProjectConfiguration, 'root'>,
+export const createNodesV2 = createImplicitLibNode({
+  pattern: 'libs/*/*/index.ts',
+  createNode({ projectRoot }) {
+    const [_, scope, shortName] = projectRoot.split('/');
+    const type = shortName.split('-')[0];
+    const name = `${scope}-${shortName}`;
+    return {
+      name,
+      projectType: 'library',
+      tags: [`scope:${scope}`, `type:${type}`, `name:${name}`],
+      targets: {
+        lint: {
+          command: 'eslint .',
+          options: {
+            cwd: projectRoot,
           },
         },
-      ] as const;
-    }),
-];
+        test: {
+          command: 'vitest',
+          options: {
+            cwd: projectRoot,
+          },
+        },
+      },
+    };
+  },
+});
+
+function createImplicitLibNode({
+  pattern,
+  createNode,
+}: {
+  pattern: string;
+  createNode: (node: {
+    projectRoot: string;
+  }) => Omit<ProjectConfiguration, 'root'>;
+}): CreateNodesV2 {
+  return [
+    pattern,
+    (filePathList) =>
+      filePathList.map((filePath) => {
+        const projectRoot = dirname(filePath);
+        return [
+          filePath,
+          {
+            projects: {
+              [projectRoot]: createNode({
+                projectRoot: projectRoot,
+              }),
+            },
+          },
+        ];
+      }),
+  ];
+}
