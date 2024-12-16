067e2da95b85af6b8f8128160f6a085a4ed7a3d6
Younes Jaaidi <younes@marmicode.io>
2024-12-16 10:12:59 +0100

    refactor: 🛠️ set up material

--- apps/whiskmate/src/app/app.config.ts
+++ apps/whiskmate/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
     provideRouter(appRoutes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
   ],
 };
