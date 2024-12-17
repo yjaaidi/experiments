bdb8c6060ccd4ed6d7ea3c660d7280f4b3515051
Younes Jaaidi <younes@marmicode.io>
2024-12-17 03:46:20 +0100

    refactor: 🛠migrate to signals & rxResource

--- apps/whiskmate/src/app/recipe-search.component.ts
+++ apps/whiskmate/src/app/recipe-search.component.ts
@@ -1,50 +1,47 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { MatProgressBar } from '@angular/material/progress-bar';
-import { suspensify } from '@jscutlery/operators';
 import {
-  BehaviorSubject,
-  combineLatest,
-  map,
-  shareReplay,
-  startWith,
-  Subject,
-  switchMap,
-} from 'rxjs';
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  linkedSignal,
+  signal,
+} from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { MatProgressBar } from '@angular/material/progress-bar';
 import { RecipeFilter } from './recipe-filter.component';
 import { Paginator } from '../shared/paginator.component';
 import { RecipePreview } from './recipe-preview.component';
 import { RecipeRepository } from './recipe-repository';
 import { Catalog } from '../shared/catalog.component';
+import { Recipe } from './recipe';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'wm-recipe-search',
   template: `
-    @if ((recipesSuspense$ | async)?.pending) {
+    @if (recipesResource.isLoading()) {
       <mat-progress-bar class="progress-bar" mode="indeterminate" />
     }
 
-    <wm-recipe-filter (keywordsChange)="keywords$.next($event)" />
+    <wm-recipe-filter [(keywords)]="keywords" />
 
-    @if (recipesSuspense$ | async; as recipesSuspense) {
-      <wm-catalog>
-        @if (recipesSuspense.hasError) {
-          <p>Something went wrong</p>
-        }
-        @if (recipesSuspense.hasValue) {
-          @for (recipe of recipesSuspense.value.items; track recipe.id) {
-            <wm-recipe-preview [recipe]="recipe" />
-          }
-        }
-      </wm-catalog>
-    }
+    <wm-catalog>
+      @if (recipesResource.error()) {
+        <p>Something went wrong</p>
+      }
+      @for (recipe of recipes(); track recipe.id) {
+        <wm-recipe-preview
+          [class.loading]="recipesResource.isLoading()"
+          [recipe]="recipe"
+          class="recipe-preview"
+        />
+      }
+    </wm-catalog>
 
     <wm-paginator
-      [itemsPerPage]="(itemsPerPage$ | async) ?? 10"
-      [offset]="(computedOffset$ | async) ?? 0"
-      [total]="(total$ | async) ?? 0"
-      (offsetChange)="offset$.next($event)"
+      [itemsPerPage]="itemsPerPage()"
+      [(offset)]="offset"
+      [total]="total()"
     />
   `,
   styles: `
@@ -54,46 +51,50 @@ import { Catalog } from '../shared/catalog.component';
       text-align: center;
     }
 
+    .loading {
+      filter: blur(3px) grayscale(80%);
+    }
+
+    .recipe-preview {
+      transition: filter 0.3s;
+    }
+
     .progress-bar {
       position: fixed;
       top: 0;
     }
   `,
-  imports: [
-    MatProgressBar,
-    RecipeFilter,
-    Catalog,
-    RecipePreview,
-    Paginator,
-    AsyncPipe,
-  ],
+  imports: [MatProgressBar, RecipeFilter, Catalog, RecipePreview, Paginator],
 })
 export class RecipeSearch {
-  keywords$ = new BehaviorSubject<string | undefined>(undefined);
-  offset$ = new Subject<number>();
-  computedOffset$ = this.keywords$.pipe(
-    switchMap(() => this.offset$.pipe(startWith(0))),
-  );
-  recipesSuspense$ = combineLatest({
-    keywords: this.keywords$,
-    offset: this.computedOffset$,
-  }).pipe(
-    switchMap(({ keywords, offset }) => {
-      return this._recipeRepository
-        .search({ keywords, offset })
-        .pipe(suspensify());
+  keywords = signal<string | undefined>(undefined);
+  offset = linkedSignal({
+    source: this.keywords,
+    computation: () => 0,
+  });
+  recipesResource = rxResource({
+    request: () => ({
+      keywords: this.keywords(),
+      offset: this.offset(),
     }),
-    shareReplay({
-      bufferSize: 1,
-      refCount: true,
+    loader: ({ request }) => this._recipeRepository.search(request),
+  });
+  recipes = linkedSignal<{ isLoading: boolean; recipes?: Recipe[] }, Recipe[]>({
+    source: () => ({
+      isLoading: this.recipesResource.isLoading(),
+      recipes: this.recipesResource.value()?.items,
     }),
+    computation: ({ isLoading, recipes }, previous) => {
+      if (isLoading) {
+        return previous?.value ?? [];
+      }
+      return recipes ?? [];
+    },
+  });
+  itemsPerPage = computed(
+    () => this.recipesResource.value()?.items.length ?? 10,
   );
-  itemsPerPage$ = this.recipesSuspense$.pipe(
-    map((suspense) => (suspense.hasValue ? suspense.value.items.length : 10)),
-  );
-  total$ = this.recipesSuspense$.pipe(
-    map((suspense) => (suspense.hasValue ? suspense.value.total : 0)),
-  );
+  total = computed(() => this.recipesResource.value()?.total ?? 0);
 
   private _recipeRepository = inject(RecipeRepository);
 }
